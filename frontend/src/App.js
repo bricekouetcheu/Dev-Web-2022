979d821acc8a521534b9b4f786de52ca5680a0b3
Liam Beaufils <liambeaufils@gmail.com>
2022-03-11 15:36:51 +0100

    Creation du router frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navigation from './pages/Navigation'
+import Homepage from './pages/Homepage';
+import Contact from './pages/Contact';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      doodie est trop bô
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+
   );
 }
 
