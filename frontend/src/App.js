124fc44d0d9c0ee29c7e1482f4f4ae524f477ad0
bricekouetcheu <brcronld@gmail.com>
2022-03-20 17:24:56 +0100

    ajout du style + responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
 import './styles/index.css'
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { BrowserRouter, Switch } from 'react-router-dom';
 
-import Navigation from './pages/Navigation';
+
 import Homepage from './pages/Homepage';
 import Contact from './pages/Contact';
 import Roman from './pages/Roman';
@@ -14,11 +13,11 @@ import Notfound from './pages/Notfound';
 
 const App = () => {
   return (
+  
     <Router>
       <div className="App">
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/Navigation" element={<Navigation />} />
           <Route path="/Contact" element={<Contact />} />
           <Route path="/Roman" element={<Roman />} />
           <Route path="/Jeunesse" element={<Jeunesse />} />
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { NavLink } from 'react-router-dom';
-import Navigation from './Navigation';
+import Navigation from '../components/Navigation';
 
 
 
