35f40083506451fe41c4bb711af3d49e17944ca6
bricekouetcheu <brcronld@gmail.com>
2022-05-19 23:07:17 +0200

    mise en place des variables d'environnement

--- Backend/databaseVersion/databasev3.sql
+++ Backend/databaseVersion/databasev3.sql
@@ -16,3 +16,28 @@ INSERT INTO comments ( description ,publisheddate, book_id,  user_id) VALUES ('t
 
 
 
+CREATE TABLE IF NOT EXISTS public.comments
+(
+    id_comment integer NOT NULL DEFAULT nextval('comments_id_comment_seq'::regclass),
+    description character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    publisheddate timestamp without time zone NOT NULL,
+    book_id character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    "parentID" integer,
+    user_id integer,
+    CONSTRAINT comments_pkey PRIMARY KEY (id_comment),
+    CONSTRAINT fk_comments FOREIGN KEY ("parentID")
+        REFERENCES public.comments (id_comment) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT fk_user FOREIGN KEY (user_id)
+        REFERENCES public.users (user_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.comments
+    OWNER to postgres;
